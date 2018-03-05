b6f513e8d6028df34433d21435aa7806641fea7b
chencheng <602530268@qq.com>
2018-03-05 18:20:15 +0800

    无限轮播

--- CCRollView_Demo/ViewController.m
+++ CCRollView_Demo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "CCRollView.h"
+#import "Masonry.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *urls = @[@"https://cdn.pixabay.com/photo/2018/01/12/10/19/fantasy-3077928_1280.jpg",
+                             @"https://cdn.pixabay.com/photo/2018/01/28/11/24/nature-3113318_1280.jpg",
+                             @"https://cdn.pixabay.com/photo/2018/02/07/14/27/pension-3137209_1280.jpg",
+                             @"https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_1280.jpg",
+                             @"https://cdn.pixabay.com/photo/2016/07/17/08/37/coins-1523383_1280.jpg"];
+    
+    NSMutableArray *imgs = @[].mutableCopy;
+    
+    NSLog(@"正在从网络加载图片...");
+    for (int i = 0; i < urls.count; i++) {
+        NSString *urlString = urls[i];
+        urlString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]];
+        UIImage *img = [UIImage imageWithData:data];
+        [imgs addObject:img];
+    }
+    
+    CCRollView *rollView = [[CCRollView alloc] initWithFrame:CGRectZero imgs:imgs];
+    [self.view addSubview:rollView];
+    [rollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+        make.size.equalTo(self.view);
+    }];
+    
+    NSLog(@"网络图片加载成功");
 }
 
 
